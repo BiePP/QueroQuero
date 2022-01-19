91653ac9922f1bed4d70da84588f94e34b1b456e
Gabriel Menezes Pereira <gabrielpereira.plumapelo@gmail.com>
2022-01-19 17:36:21 -0300

    Flappy: HUD inicial adicionado  + danos.

--- Assets/GameFolder/Flappy/QueroFlappy/Script/FlappyController.cs
+++ Assets/GameFolder/Flappy/QueroFlappy/Script/FlappyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlappyController : MonoBehaviour
 {
@@ -9,11 +10,19 @@ public class FlappyController : MonoBehaviour
 
     public float jumpForce;
 
+    public int life;
+    bool alive;
+
+    public Text healthCountText;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+
+        alive = true;
+        healthCountText.text = life.ToString();
     }
 
     // Update is called once per frame
@@ -27,7 +36,28 @@ public class FlappyController : MonoBehaviour
             rb.AddForce(new Vector2(0, jumpForce));
         }
 
+        if(life == 0 && alive)
+        {
+            alive = false;
+            StartCoroutine(QueroQueroFall());
+        }
 
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            life--;
+            healthCountText.text = life.ToString();
+        }
+    }
+
+    private IEnumerator QueroQueroFall()
+    {
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 1.5f;
+        yield return new WaitForSeconds(2f);
+        Time.timeScale = 0;
+    }
 }
